e489bfdf9a0455a39e91386daa2a02ba859ab4fb
2mingyu <tnt01010@naver.com>
2024-05-17 13:23:30 +0900

    fix: PLZNFT methods override

--- contracts/4_PLZNFT.sol
+++ contracts/4_PLZNFT.sol
@@ -3,18 +3,37 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 
 contract PLZNFT is ERC721URIStorage, Ownable(msg.sender) {
     uint256 private _tokenIdCounter = 1;
 
     constructor() ERC721("PLZNFT", "PLZNFT") {}
 
-    // renounceOwnership 함수를 오버라이드하여 비활성화
-    function renounceOwnership() view public override onlyOwner {
-        // 아무런 작업도 수행하지 않거나 예외를 발생시키기
-        revert("Renouncing ownership is disabled.");
+    // approve를 오버라이드하여 비활성화
+    function approve(address /*to*/, uint256 /*tokenId*/) public view override(ERC721, IERC721) onlyOwner {
+        revert("Approve function is disabled.");
     }
 
+    // setApprovalForAll을 오버라이드하여 비활성화
+    function setApprovalForAll(address /*operator*/, bool /*approved*/) public view override(ERC721, IERC721) onlyOwner {
+        revert("Set approval for all function is disabled.");
+    }
+
+    // transferFrom을 오버라이드하여 비활성화
+    function transferFrom(address /*from*/, address /*to*/, uint256 /*tokenId*/) public view override(ERC721, IERC721) onlyOwner {
+        revert("Transfer from function is disabled.");
+    }
+
+    // transferOwnership을 오버라이드하여 비활성화 또는 소유자만 가능하도록 수정
+    function transferOwnership(address /*newOwner*/) public view override onlyOwner {
+        revert("Transfer ownership function is disabled.");
+    }
+
+    // renounceOwnership 함수를 오버라이드하여 비활성화
+    function renounceOwnership() view public override onlyOwner {
+        revert("Renouncing ownership is disabled.");
+    }
 
     // NFT를 민팅하고 바로 다른 주소로 전송하는 함수
     // 컨트랙트 소유자만 호출 가능 (onlyOwner)
